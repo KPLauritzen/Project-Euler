7297c03a690766774620df3ec658f312812ec8b5
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2013-01-30 18:28:02 -0800

    Problem 81

---
+++ python/problem-81/main.py
@@ -0,0 +1,42 @@
+from pylab import *
+import networkx as nx
+
+f = open("matrix.txt","r")
+
+mat = []
+for line in f:
+    mat.append(line.split(","))
+# use numpy array
+mat = array(mat) 
+# change from strings to ints
+mat = mat.astype(int) 
+origMat = mat.copy()
+
+# mat = [
+# [131,	673,	234,	103,	18],
+# [201,	96,	342,	965,	150],
+# [630,	803,	746,	422,	111],
+# [537,	699,	497,	121,	956],
+# [805,	732,	524,	37,	331]
+# ]
+# # use numpy array
+# mat = array(mat) 
+# # change from strings to ints
+# mat = mat.astype(int) 
+
+
+size = 80
+# Testing graphs with networkx
+G = nx.DiGraph()
+
+for i in range(size):
+    for j in range(size):
+        G.add_edge((i,j), (i+1,j), weight=mat[i,j])
+        G.add_edge((i,j), (i,j+1), weight=mat[i,j])
+
+
+for i in range(size):
+    G.remove_node((i,size))
+    G.remove_node((size,i))
+
+
